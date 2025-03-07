e87c7dc22ed49d33481f237ed1844b80c4d053d1
priti200 <pritugupta2001@gmail.com>
2025-03-08 00:14:44 +0530

    Set-Up the backend of Hoperlens project using Spring-boot

---
+++ Hopelens/src/test/java/com/Hopelens/HopelensApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Hopelens;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HopelensApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
